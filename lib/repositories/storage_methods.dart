5ab8f08d1b51eca431afe1b2772d7a59b3e7a067
Ramodise Mosuoe <ramodisemosuoe@gmail.com>
2022-10-23 17:52:14 +0200

    struggling with null safety again

--- lib/repositories/storage_methods.dart
+++ lib/repositories/storage_methods.dart
@@ -13,7 +13,7 @@ class StorageMethods {
     // creating location to our firebase storage
 
     Reference ref =
-    _storage.ref().child(childName).child(_auth.currentUser.uid) //xxbb
+    _storage.ref().child(childName).child(_auth.currentUser.uid) //xxbbyyy
     if(isPost) {
       String id = const Uuid().v1();
       ref = ref.child(id);
